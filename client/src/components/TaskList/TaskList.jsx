a938bf308c251e8de1949094f3f5ad04479f7313
keinich <rafael.weschle@gmail.com>
2022-02-07 12:59:35 +0100

    Tasks Set Done

--- client/src/components/TaskList/TaskList.jsx
+++ client/src/components/TaskList/TaskList.jsx
@@ -2,9 +2,11 @@ import React, { Fragment, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { styled } from "@mui/material/styles";
 
-import { Paper, Grid, IconButton, Box } from "@mui/material";
+import { Paper, Grid, IconButton, Box, Button } from "@mui/material";
 import BuildIcon from "@mui/icons-material/Build";
 import DeleteIcon from "@mui/icons-material/Delete";
+import ArrowDropDownOutlinedIcon from "@mui/icons-material/ArrowDropDownOutlined";
+import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
 
 import { getTasks } from "../../actions/taskActions";
 import Task from "../Task/Task";
@@ -13,16 +15,40 @@ import "./TaskList.css";
 
 const TaskList = () => {
   const tasks = useSelector((state) => state.tasks);
+  const [showDone, setShowDone] = useState(true);
 
   return (
     <>
       <Fragment>
         <Box container spacing={0} className="tasklist__grid">
-          {/* <Grid item xs={12}> */}
-          {Object.keys(tasks).map((key) => (
-            <Task task={tasks[key]} key={key} />
-          ))}
-          {/* </Grid> */}
+          {Object.keys(tasks).map(
+            (key) => !tasks[key].done && <Task task={tasks[key]} key={key} />
+          )}
+          {showDone ? (
+            <Button
+              sx={{ marginTop: "20px" }}
+              variant="contained"
+              startIcon={<ArrowDropDownOutlinedIcon />}
+              onClick = {() => setShowDone(false)}
+            >
+              {" "}
+              Done
+            </Button>
+          ) : (
+            <Button
+              sx={{ marginTop: "20px" }}
+              variant="contained"
+              startIcon={<ArrowRightOutlinedIcon />}
+              onClick = {() => setShowDone(true)}
+            >
+              {" "}
+              Done
+            </Button>
+          )}
+
+          {showDone && Object.keys(tasks).map(
+            (key) => tasks[key].done && <Task task={tasks[key]} key={key} />
+          )}
         </Box>
       </Fragment>
     </>
