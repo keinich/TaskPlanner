1c9a10b5a2e3146f822d281564dfc633f7c2c43c
keinich <rafael.weschle@gmail.com>
2022-02-07 20:44:26 +0100

    deploy stuff

--- db.js
+++ db.js
@@ -1,15 +1,22 @@
 import pg1 from "pg";
+import * as dotenv from 'dotenv';
 
+const env = dotenv.config().parsed;
 const Pool = pg1.Pool;
 
-// const Pool = require("pg").Pool;
+const devConfig = {
+  user: env.PG_USER,
+  password: env.PG_PASSWORD,
+  host: env.PG_HOST,
+  port: env.PG_PORT,
+  database: env.PG_DATABASE,
+}
 
-const pool = new Pool({
-  user: "postgres",
-  password: "password",
-  host: "localhost",
-  port: 5432,
-  database: "taskplanner",
-});
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL // heroku addon
+}
+
+// console.log("env", env.NODE_ENV);
+const pool = new Pool(env.NODE_ENV === "production" ? prodConfig : devConfig);
 
 export default pool;
--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
